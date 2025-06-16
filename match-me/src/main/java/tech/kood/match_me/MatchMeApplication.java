072d6781a4780c490cfe3aa4948a5c19a67609ae
Rivo Tüksammel <rivo.tuksammel@gmail.com>
2025-06-16 14:58:26 +0000

    Update environment variables and clean up dependencies
    - Changed HOST_USERNAME to 'rivo' and updated HOST_DOCKER_GID to 1001 in .env file.
    - Removed unused dependencies from pom.xml.
    - Excluded DataSourceAutoConfiguration in MatchMeApplication.
    - Added VSCode settings.json for Java configuration.
    - Initialized javadoc.json file.

--- match-me/src/main/java/tech/kood/match_me/MatchMeApplication.java
+++ match-me/src/main/java/tech/kood/match_me/MatchMeApplication.java
@@ -2,12 +2,12 @@ package tech.kood.match_me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MatchMeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchMeApplication.class, args);
 	}
-
 }
