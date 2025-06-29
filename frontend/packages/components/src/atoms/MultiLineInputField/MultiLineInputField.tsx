3c6ec988ed0e800d553f8a2cd434af5ce3958761
kati.kraavi <katikraavi@gmail.com>
2025-06-29 09:13:39 +0000

    no label in multiline inputfield

--- frontend/packages/components/src/atoms/MultiLineInputField/MultiLineInputField.tsx
+++ frontend/packages/components/src/atoms/MultiLineInputField/MultiLineInputField.tsx
@@ -11,12 +11,12 @@ interface MultiLineInputFieldProps {
 
 export const MultiLineInputField = ({ id, placeholder, value, onChange, maxLength = 250, rows = 4 }: MultiLineInputFieldProps) => (
   <textarea
-    id={id}
-    placeholder={placeholder}
-    value={value}
-    onChange={onChange}
-    maxLength={maxLength}
-    rows={rows}
-    className="w-full px-4 py-2 border-gray-300 rounded shadow-md shadow-gray-400 focus:outline-none resize-none bg-accent-light"
-  />
+  id={id}
+  placeholder={placeholder}
+  value={value}
+  onChange={onChange}
+  maxLength={maxLength}
+  rows={rows}
+  className="w-full px-4 py-2 border-gray-300 rounded shadow-md shadow-gray-400 focus:outline-none resize-none bg-accent-light"
+/>
 );
