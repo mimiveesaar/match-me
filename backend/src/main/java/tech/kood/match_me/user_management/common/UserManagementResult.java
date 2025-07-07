1d5ac4e46d92b2e738fdbb3cc3a4de51c5428498
Rivo Tüksammel <rivo.tuksammel@gmail.com>
2025-07-07 12:58:02 +0300

    feat: refactor user management module by removing unused classes and adding new user registration features

---
+++ backend/src/main/java/tech/kood/match_me/user_management/common/UserManagementResult.java
@@ -0,0 +1,8 @@
+package tech.kood.match_me.user_management.common;
+
+
+public interface UserManagementResult {
+    // Marker interface for user management results
+
+    public String tracingId();
+}
---
+++ backend/src/main/java/tech/kood/match_me/user_management/features/registerUser/results/RegisterUserResult.java
@@ -0,0 +1,39 @@
+package tech.kood.match_me.user_management.features.registerUser.results;
+
+// Sealed interface for oneof result
+
+import tech.kood.match_me.user_management.common.UserManagementResult;
+
+public sealed interface RegisterUserResult extends UserManagementResult
+    permits RegisterUserResult.Success, RegisterUserResult.UsernameExists,
+        RegisterUserResult.EmailExists, RegisterUserResult.InvalidEmail {
+
+    record Success(String userId, String tracingId) implements RegisterUserResult {
+            @Override
+            public String tracingId() {
+                return tracingId;
+            }
+    }
+
+    record UsernameExists(String username, String tracingId)
+        implements RegisterUserResult {
+            @Override
+            public String tracingId() {
+                return tracingId;
+            }
+        }
+
+    record EmailExists(String email, String tracingId) implements RegisterUserResult {
+            @Override
+            public String tracingId() {
+                return tracingId;
+            }
+    }
+
+    record InvalidEmail(String email, String tracingId) implements RegisterUserResult {
+            @Override
+            public String tracingId() {
+                return tracingId;
+            }
+    }
+}
