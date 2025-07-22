0b9e788e975eddbe5cbcc15b82e84ef78c1a802d
Rivo Tüksammel <rivo.tuksammel@gmail.com>
2025-07-22 15:17:12 +0300

    feat: enhance styling and layout of LogIn component for improved user experience

--- frontend/packages/components/src/organisms/UserLogInForm/UserLogInForm.tsx
+++ frontend/packages/components/src/organisms/UserLogInForm/UserLogInForm.tsx
@@ -13,22 +13,26 @@ export const LogIn = () => {
 
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <CircleWrapper className="bg-amberglow rounded-2xl p-8 text-center">
-        <div className="flex w-full flex-col items-center p-10">
-            <h2 className="mb-5 text-2xl font-semibold">You have returned!</h2>
-            <LabeledInputField
-              label="Username"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <LabeledInputField
-              label="Password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+      <CircleWrapper className="bg-amberglow relative rounded-2xl p-12 text-center md:p-58 lg:p-64">
+        <div className="flex w-full flex-col items-center py-10 md:absolute md:p-20">
+          <h2 className="mb-5 text-2xl font-semibold md:mt-2 md:text-3xl">
+            You have returned!
+          </h2>
+          <LabeledInputField
+            label="Username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <LabeledInputField
+            label="Password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="mt-1">
             <KeyIcon onClick={() => console.log({ username })} />
+          </div>
         </div>
       </CircleWrapper>
     </div>
