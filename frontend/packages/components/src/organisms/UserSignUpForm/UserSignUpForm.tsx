b5e95306a91a8fa4f9d67d3ec8935a5aab1ac9e7
Rivo Tüksammel <rivo.tuksammel@gmail.com>
2025-07-22 16:35:41 +0300

    feat: update NextPageIconButton styling and enhance UserSignUpForm layout for better user experience

--- frontend/packages/components/src/organisms/UserSignUpForm/UserSignUpForm.tsx
+++ frontend/packages/components/src/organisms/UserSignUpForm/UserSignUpForm.tsx
@@ -14,33 +14,31 @@ export const UserSignUpForm = () => {
 
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <CircleWrapper className="bg-minty h-[600px] w-[600px] p-8 text-center">
-        <div className="w-full px-6 py-8 text-center">
-          <div className="flex w-full flex-col items-center">
-            <h2 className="mb-6 text-lg font-semibold">New Account</h2>
-            <LabeledInputField
-              label="Username"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <LabeledInputField
-              label="E-mail"
-              placeholder="E-mail"
-              value={mail}
-              onChange={(e) => setMail(e.target.value)}
-            />
-            <LabeledInputField
-              label="Password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+      <CircleWrapper className="bg-minty relative rounded-2xl p-12 text-center md:p-60 lg:p-64">
+        <div className="flex w-full flex-col items-center py-10 md:absolute md:p-20">
+          <h2 className="text-2xl mb-5 font-semibold md:mt-2 md:text-3xl">
+            New Account
+          </h2>
+          <LabeledInputField
+            label="Username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <LabeledInputField
+            label="E-mail"
+            placeholder="E-mail"
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+          />
+          <LabeledInputField
+            label="Password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-            <NextPageIconButton
-              onClick={() => console.log({ username, mail })}
-            />
-          </div>
+          <NextPageIconButton onClick={() => console.log({ username, mail })} />
         </div>
       </CircleWrapper>
     </div>
