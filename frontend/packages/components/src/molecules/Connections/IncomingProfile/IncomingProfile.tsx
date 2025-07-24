6dc5d84c83e97fefa139cc52f8246d4473d33250
Rivo Tüksammel <rivo.tuksammel@gmail.com>
2025-07-24 18:22:42 +0300

    style: Username black text color

--- frontend/packages/components/src/molecules/Connections/IncomingProfile/IncomingProfile.tsx
+++ frontend/packages/components/src/molecules/Connections/IncomingProfile/IncomingProfile.tsx
@@ -23,7 +23,7 @@ export const IncomingProfile = ({
     <div className="flex w-full max-w-96 items-center justify-between px-1 py-2">
       <div className="flex items-center justify-center space-x-4">
         <RoundProfilePicture imageUrl={profilePictureUrl} />
-        <Username username={username} />
+        <Username username={username} className="!text-black" />
       </div>
       <div className="flex items-center justify-center space-x-2">
         <AcceptButton onClick={onAccept} />
