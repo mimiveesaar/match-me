8b091a7c9edf4e9ccff69d0d8f6039d99ca77fb8
kati.kraavi <katikraavi@gmail.com>
2025-08-21 15:19:51 +0000

    aplication no flyway

--- backend/src/main/java/tech/kood/match_me/profile/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package tech.kood.match_me.profile.repository;
-
-import java.lang.reflect.AccessFlag.Location;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface LocationRepository extends JpaRepository<Location, Long> {}
