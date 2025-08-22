91509fae13e999c9d8d21fb8d342fba20ed1c941
kati.kraavi <katikraavi@gmail.com>
2025-08-22 08:17:14 +0000

    security enabled

--- backend/src/test/java/tech/kood/match_me/user_management/UserManagementTestApplication.java
+++
@@ -1,7 +0,0 @@
-package tech.kood.match_me.user_management;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "tech.kood.match_me.user_management")
-public class UserManagementTestApplication {
-}
