a6ab8e728f8a747e80322c62861034e3383b3e00
Rivo Tuksammel <rivo.tuksammel@gmail.com>
2025-08-25 20:45:16 +0300

    Temporarily remove connections module.

--- backend/src/test/java/tech/kood/match_me/connections/ConnectionsTestApplication.java
+++
@@ -1,8 +0,0 @@
-package tech.kood.match_me.connections;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = {"tech.kood.match_me.connections", "tech.kood.match_me.common"})
-public class ConnectionsTestApplication {
-
-}
