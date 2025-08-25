e1b1aaf00169e30a771c0f0f89247c0dbe24a24c
Rivo Tuksammel <rivo.tuksammel@gmail.com>
2025-08-25 20:09:41 +0300

    Fix IaC initialization script.

--- backend/src/main/java/tech/kood/match_me/connections/internal/features/acceptConnection/AcceptConnectionRequest.java
+++
@@ -1,7 +0,0 @@
-package tech.kood.match_me.connections.internal.features.acceptConnection;
-
-import java.io.Serializable;
-
-public record AcceptConnectionRequest(String requestId, String connectionRequestId, String tracingId)
-        implements Serializable {
-}
