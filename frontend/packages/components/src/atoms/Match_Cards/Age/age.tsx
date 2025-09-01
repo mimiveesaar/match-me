1f85d4c633857d94d24e30fcafaaa09d2ff1b72d
Mimi Veesaar <mimi.ladva@gmail.com>
2025-09-01 19:06:48 +0000

    shadows added

--- frontend/packages/components/src/atoms/Match_Cards/Age/age.tsx
+++ frontend/packages/components/src/atoms/Match_Cards/Age/age.tsx
@@ -7,7 +7,7 @@ interface AgeProps {
 }
 
 export const Age = ({ age }: AgeProps) => (
-    <span className="text-ivory text-2xl font-medium font-ibm_plex_sans">
+    <span className="text-ivory text-2xl font-semibold font-ibm_plex_sans drop-shadow-custom-2">
       {age}
     </span>
 )
--- frontend/packages/components/src/atoms/Match_Cards/UserName/username.tsx
+++ frontend/packages/components/src/atoms/Match_Cards/UserName/username.tsx
@@ -9,7 +9,7 @@ interface UsernameProps {
 export const Username = ({ username }: UsernameProps) => {
   
   return (
-    <span className="text-ivory text-2xl font-medium font-ibm_plex_sans">
+    <span className="text-ivory text-2xl font-medium font-ibm_plex_sans drop-shadow-custom-2">
       {username}
     </span>
   );
