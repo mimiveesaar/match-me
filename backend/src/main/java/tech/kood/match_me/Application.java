26988edf906989d94af55a0096fdac51113b41e1
Rivo Tuksammel <rivo.tuksammel@gmail.com>
2025-09-10 12:18:49 +0300

    Switch away from Artemis because it's a buggy mess.

--- backend/src/main/java/tech/kood/match_me/Application.java
+++ backend/src/main/java/tech/kood/match_me/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
-@EnableJms
 public class Application {
 
 	public static void main(String[] args) {
