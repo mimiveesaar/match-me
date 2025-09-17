a124c600270d5ec08da4c5550b7b2713253b8e4b
Mimi Veesaar <mimi.ladva@gmail.com>
2025-09-17 12:27:54 +0300

    Remove unused JPA configuration annotations from Application class

--- backend/src/main/java/tech/kood/match_me/Application.java
+++ backend/src/main/java/tech/kood/match_me/Application.java
@@ -2,16 +2,12 @@ package tech.kood.match_me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
-@EnableJpaRepositories(basePackages = "tech.kood.match_me.matching.repository")
-@EntityScan(basePackages = "tech.kood.match_me.matching.model")
 public class Application {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
