bc431640737e7a57abdbad4e67427bd66d19260f
Rivo Tuksammel <rivo.tuksammel@gmail.com>
2025-09-19 08:02:46 +0300

    Refactor MatchCardButtons: reintroduce ButtonTriangle component and update stories

---
+++ frontend/packages/components/src/molecules/Connections/index.ts
@@ -0,0 +1,6 @@
+export * from './ConnectionProfile';
+export * from './IncomingPingsHeader';
+export * from './IncomingProfile';
+export * from './MyConnectionsHeader';
+export * from './OutgoingPingsHeader';
+export * from './OutgoingProfile';
