9fe15aa1e0c93ad7f258c37d78167b73b227fe69
mimiveesaar <mimiveesaar@gmail.com>
2025-10-13 10:32:51 +0300

    incoming user form  profile test

--- backend/src/main/java/tech/kood/match_me/matching/listeners/IncomingUserEventListener.java
+++ backend/src/main/java/tech/kood/match_me/matching/listeners/IncomingUserEventListener.java
@@ -1,11 +1,23 @@
 package tech.kood.match_me.matching.listeners;
 
+import org.springframework.context.event.EventListener;
+import tech.kood.match_me.matching.model.User;
 import tech.kood.match_me.matching.repository.MatchUserRepository;
-import tech.kood.match_me.user_management.features.user.internal.persistance.UserRepository;
+import tech.kood.match_me.profile.events.ProfileChangedEvent;
 
 public class IncomingUserEventListener {
 
-    private final MatchUserRepository matchingUserRepository;
+    private final MatchUserRepository matchUserRepository;
+
+    public IncomingUserEventListener(MatchUserRepository matchUserRepository) {
+        this.matchUserRepository = matchUserRepository;
+    }
+
+    @EventListener
+    public void handleProfileChanged(ProfileChangedEvent event) {
+        User user = event.getUser();
+        matchUserRepository.save(user);
+    }
 
 
 }
