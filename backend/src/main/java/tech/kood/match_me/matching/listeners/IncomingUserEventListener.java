79f9c6c8ac54db261cfb80cb8d13ffb9d926c371
mimiveesaar <mimiveesaar@gmail.com>
2025-10-13 10:45:28 +0300

    incoming user form  profile test

--- backend/src/main/java/tech/kood/match_me/matching/listeners/IncomingUserEventListener.java
+++ backend/src/main/java/tech/kood/match_me/matching/listeners/IncomingUserEventListener.java
@@ -1,9 +1,13 @@
 package tech.kood.match_me.matching.listeners;
 
 import org.springframework.context.event.EventListener;
-import tech.kood.match_me.matching.model.User;
+import tech.kood.match_me.matching.model.*;
 import tech.kood.match_me.matching.repository.MatchUserRepository;
 import tech.kood.match_me.profile.events.ProfileChangedEvent;
+import tech.kood.match_me.profile.model.Profile;
+
+import java.util.HashSet;
+import java.util.UUID;
 
 public class IncomingUserEventListener {
 
@@ -15,7 +19,16 @@ public class IncomingUserEventListener {
 
     @EventListener
     public void handleProfileChanged(ProfileChangedEvent event) {
-        User user = event.getUser();
+
+        var profile = event.getProfile();
+
+        var bodyformEntity = new Bodyform(profile.getBodyform().getId());
+        var homePlanetEntity = new Homeplanet(1, "Earth", 12.1, 13.1 );
+        var lookingForEntity = new LookingFor(1, "...");
+        var interestsEntity = new Interest( "kkk");
+        var interestsSetEntity = new HashSet<Interest>();
+        var userEntity = new User(UUID.randomUUID(),"Mimi", 27, homePlanetEntity, bodyformEntity, "mmm", lookingForEntity, interestsSetEntity, "");
+
         matchUserRepository.save(user);
     }
 
--- backend/src/main/java/tech/kood/match_me/profile/package-info.java
+++ backend/src/main/java/tech/kood/match_me/profile/package-info.java
@@ -1,3 +1,5 @@
 @org.jspecify.annotations.NullMarked
-@org.springframework.modulith.ApplicationModule
+@org.springframework.modulith.ApplicationModule(type = ApplicationModule.Type.OPEN)
 package tech.kood.match_me.profile;
+
+import org.springframework.modulith.ApplicationModule;
