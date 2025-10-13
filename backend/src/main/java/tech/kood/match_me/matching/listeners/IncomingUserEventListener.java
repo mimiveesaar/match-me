5d6461538192a630454b76d0ad46b88d05541f66
mimiveesaar <mimiveesaar@gmail.com>
2025-10-13 10:24:59 +0300

    incoming user form  profile test

---
+++ backend/src/main/java/tech/kood/match_me/matching/listeners/IncomingUserEventListener.java
@@ -0,0 +1,11 @@
+package tech.kood.match_me.matching.listeners;
+
+import tech.kood.match_me.matching.repository.MatchUserRepository;
+import tech.kood.match_me.user_management.features.user.internal.persistance.UserRepository;
+
+public class IncomingUserEventListener {
+
+    private final MatchUserRepository matchingUserRepository;
+
+
+}
--- backend/src/test/java/tech/kood/match_me/matching/event/IncomingProfileEventTest.java
+++ backend/src/test/java/tech/kood/match_me/matching/event/IncomingProfileEventTest.java
@@ -1,4 +1,4 @@
-package tech.kood.match_me.matching.repository;
+package tech.kood.match_me.matching.event;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,34 +6,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import tech.kood.match_me.matching.common.MatchingTestBase;
 import tech.kood.match_me.matching.model.*;
+import tech.kood.match_me.matching.repository.MatchUserRepository;
+import tech.kood.match_me.profile.events.ProfileChangedEvent;
 
 import java.util.HashSet;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 @Transactional
-public class MatchUserRepositoryTest extends MatchingTestBase {
+public class IncomingProfileEventTest extends MatchingTestBase {
 
     @Autowired
     private MatchUserRepository matchUserRepository;
 
     @Test
-    void testSaveAndFindById() {
+    void testListenAndSaveNewUser() {
 
         var bodyformEntity = new Bodyform(1, "gelatinous");
         var homePlanetEntity = new Homeplanet(1, "Earth", 12.1, 13.1 );
         var lookingForEntity = new LookingFor(1, "...");
         var interestsEntity = new Interest( "kkk");
         var interestsSetEntity = new HashSet<Interest>();
-        var userEntity = new User(UUID.randomUUID(),"Mimi", 27, homePlanetEntity, bodyformEntity, "mmm", lookingForEntity, interestsSetEntity, "");
+        var newUserEntity = new User(UUID.randomUUID(),"Mimi", 27, homePlanetEntity, bodyformEntity, "mmm", lookingForEntity, interestsSetEntity, "");
 
-        matchUserRepository.save(userEntity);
+        var newProfileSent = new ProfileChangedEvent(newUserEntity);
 
-        var queryResult = matchUserRepository.findByHomeplanetId(homePlanetEntity.getId());
-
-        assertEquals( 1, queryResult.size());
+        // Kuidas ma kuulan eventi?
+        matchUserRepository.save(newUserEntity);
     }
-
 }
