938a903fb41d52b1de8c074bbdaeebdbd9c00c93
Rivo Tuksammel <rivo.tuksammel@gmail.com>
2025-10-15 11:02:59 +0300

    Fixes.

--- backend/src/main/java/tech/kood/match_me/seeding/SeedUserGenerator.java
+++ backend/src/main/java/tech/kood/match_me/seeding/SeedUserGenerator.java
@@ -9,50 +9,37 @@ import tech.kood.match_me.user_management.features.user.domain.UserStateDTO;
 
 @Component
 public class SeedUserGenerator {
+    private static final int USER_COUNT = 100;
 
-    //TODO: make hashed passwords realistic.
+    private static class FakeUserData {
+        final String id;
+        final String email;
+        final String passwordHash;
+        final String salt;
+        final int state;
+
+        FakeUserData(int index) {
+            this.id = String.format("%08d-%04d-%04d-%04d-%012d", index, index, index, index, index);
+            this.email = String.format("user%d@example.com", index + 1);
+            this.passwordHash = "hash" + (index + 1);
+            this.salt = "salt" + (index + 1);
+            this.state = (index % 2 == 0) ? 1 : 0;
+        }
+    }
+
     public UserDTO[] generate() {
-        return new UserDTO[] {
-            new UserDTO(
-                UserIdDTO.of("11111111-1111-1111-1111-111111111111"),
-                EmailDTO.of("user1@example.com"),
-                new HashedPasswordDTO("hash1", "salt1"),
-                new UserStateDTO(1),
+        UserDTO[] users = new UserDTO[USER_COUNT];
+        for (int i = 0; i < USER_COUNT; i++) {
+            FakeUserData data = new FakeUserData(i);
+            users[i] = new UserDTO(
+                UserIdDTO.of(data.id),
+                EmailDTO.of(data.email),
+                new HashedPasswordDTO(data.passwordHash, data.salt),
+                new UserStateDTO(data.state),
                 java.time.Instant.now(),
                 java.time.Instant.now()
-            ),
-            new UserDTO(
-                UserIdDTO.of("22222222-2222-2222-2222-222222222222"),
-                EmailDTO.of("user2@example.com"),
-                new HashedPasswordDTO("hash2", "salt2"),
-                new UserStateDTO(1),
-                java.time.Instant.now(),
-                java.time.Instant.now()
-            ),
-            new UserDTO(
-                UserIdDTO.of("33333333-3333-3333-3333-333333333333"),
-                EmailDTO.of("user3@example.com"),
-                new HashedPasswordDTO("hash3", "salt3"),
-                new UserStateDTO(0),
-                java.time.Instant.now(),
-                java.time.Instant.now()
-            ),
-            new UserDTO(
-                UserIdDTO.of("44444444-4444-4444-4444-444444444444"),
-                EmailDTO.of("user4@example.com"),
-                new HashedPasswordDTO("hash4", "salt4"),
-                new UserStateDTO(0),
-                java.time.Instant.now(),
-                java.time.Instant.now()
-            ),
-            new UserDTO(
-                UserIdDTO.of("55555555-5555-5555-5555-555555555555"),
-                EmailDTO.of("user5@example.com"),
-                new HashedPasswordDTO("hash5", "salt5"),
-                new UserStateDTO(0),
-                java.time.Instant.now(),
-                java.time.Instant.now()
-            )
-        };
+            );
+        }
+        return users;
     }
 }
